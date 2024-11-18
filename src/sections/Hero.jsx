910dbbb3d1e0993ab9153cafa3e625c743c11fb5
mac gr <macgr@macs-MacBook-Air.local>
2024-11-18 22:38:57 +0100

    version 4

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,17 +19,17 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 pt-12 sm:pt-0">
         <motion.div      
               initial={{  opacity: 0, y: -30 }}
               animate={{  opacity: 1, y: 0 }}
               
             >
-          <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans ">
+          <p className="sm:text-4xl text-2xl font-medium text-white text-center font-generalsans ">
             Hi, I am A_ELBOUZARI  <span className="waving-hand">👋</span>
           </p>
         </motion.div>
-        <p className="hero_tag text-gray_gradient">Electronics , IOT , Robotics , FullStack Web </p>
+        <p className="hero_tag text-gray_gradient ">Electronics , IOT , Robotics , FullStack Web </p>
       </div>
 
       <div className="w-full h-full absolute inset-0">

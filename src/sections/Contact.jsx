248ebf205e5e3fbc29ef4c83b16a0ce92609cb6b
AYOUB ELBOUZARI <153466765+Digitalbozr@users.noreply.github.com>
2025-01-19 17:18:09 +0100

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,16 +22,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+       "service_ws9huif",
+        "__ejs-test-mail-service__",
         {
           from_name: form.name,
           to_name: 'ayoubelbouzari',
           from_email: form.email,
           to_email: 'topayoubozr01@gmail.com',
-          message: form.message,
+          message: form.message,'
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        "_a9-YuBycErQfmBo-",
       )
       .then(
         () => {
